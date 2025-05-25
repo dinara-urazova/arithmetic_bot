2fbb619c09ba1bede82ed9c80c25189fc609c9c1
Dinara Urazova <dinara.urazova88@gmail.com>
2025-05-25 17:03:31 +0500

    replace redundant code (send_msg func), add checks

--- bot/__main__.py
+++ bot/__main__.py
@@ -15,7 +15,15 @@ def delete_webhook():  # сбоит без этой функции через к
         print("Failed to delete webhook:", data)
 
 
+def send_message(chat_id, text):
+    requests.post(
+        f"https://api.telegram.org/bot{token}/sendMessage",
+        json={"chat_id": chat_id, "text": text},
+    )
+
+
 delete_webhook()
+
 next_update_id = 0
 users = {}
 
@@ -34,69 +42,44 @@ while True:
         updates = data["result"]
 
         for update in updates:
+            if "message" not in update:
+                continue 
             next_update_id = update["update_id"] + 1
+            if "text" not in update["message"]:
+                continue
             text = update["message"]["text"]
             chat_id = update["message"]["chat"]["id"]
 
-            if chat_id not in users:
-                users[chat_id] = {"step": "ask_first"}
-
-            state = users[chat_id]
-
             if text == "/start":
                 users[chat_id] = {"step": "ask_first"}
-                requests.post(
-                    f"https://api.telegram.org/bot{token}/sendMessage",
-                    json={
-                        "chat_id": chat_id,
-                        "text": "Добро пожаловать! Пожалуйста, введите первое число.",
-                    },
-                )
+                send_message(chat_id, "Добро пожаловать! Пожалуйста, введите первое число.")
+                continue
 
-            elif state["step"] == "ask_first":
+            if chat_id not in users:
+                send_message(chat_id, "Пожалуйста, введите команду /start")
+                continue
+
+            state = users[chat_id]    
+            if state["step"] == "ask_first":
                 try:
                     first_number = float(text)
                     state["first_num"] = first_number
                     state["step"] = "ask_second"
-                    requests.post(
-                        f"https://api.telegram.org/bot{token}/sendMessage",
-                        json={
-                            "chat_id": chat_id,
-                            "text": "Принял, спасибо! Можете вводить второе число.",
-                        },
-                    )
+                    send_message(chat_id, "Принял, спасибо! Можете вводить второе число.")
+        
                 except ValueError:
-                    requests.post(
-                        f"https://api.telegram.org/bot{token}/sendMessage",
-                        json={
-                            "chat_id": chat_id,
-                            "text": "Пожалуйста, введите число в правильном формате.",
-                        },
-                    )
+                    send_message(chat_id, "Пожалуйста, введите число в правильном формате.")
 
             elif state["step"] == "ask_second":
                 try:
                     second_number = float(text)
                     total_sum = state["first_num"] + second_number
-                    requests.post(
-                        f"https://api.telegram.org/bot{token}/sendMessage",
-                        json={
-                            "chat_id": chat_id,
-                            "text": f"Спасибо, принял! Сумма двух чисел - {total_sum}",
-                        },
-                    )
+                    send_message(chat_id, f"Спасибо, принял! Сумма двух чисел - {total_sum}")
                     # users = {12345: {'step': 'ask_second', 'first_num': 3.0}} - так выглядит словарь users после запроса 2 числа
-                    del users[
-                        chat_id
-                    ]  # удаляем данные о пользователе, чтобы мб заново ввести числа, словарь пустой -> users = {}
+                    del users[chat_id]  # удаляем данные о пользователе, чтобы мб заново ввести числа, словарь пустой -> users = {}
                 except ValueError:
-                    requests.post(
-                        f"https://api.telegram.org/bot{token}/sendMessage",
-                        json={
-                            "chat_id": chat_id,
-                            "text": "Пожалуйста, введите число в правильном формате.",
-                        },
-                    )
+                    send_message(chat_id, "Пожалуйста, введите число в правильном формате.")
+                    
     except Exception as e:
         print(f"The error is {e}")
 
